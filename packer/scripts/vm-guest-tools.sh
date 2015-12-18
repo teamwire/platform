5cea7894fdf87ad25392d4f865e4e0402450d9bd
Joachim de Groot <joachim@teamwire.eu>
2015-12-18 11:59:57 +0100

    Install open-vm-tools on VMware guests

--- packer/scripts/vm-guest-tools.sh
+++ packer/scripts/vm-guest-tools.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
+if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+	echo "Installing VMware Tools"
+	sudo apt-get install -y --no-install-recommends open-vm-tools
+elif [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
 	echo "Installing VirtualBox Guest Additions"
-
 	sudo apt-get install -y --no-install-recommends virtualbox-guest-dkms virtualbox-guest-utils
 fi
