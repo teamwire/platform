bf77318543d18eed5d473141a9478465988e2fd7
Joachim de Groot <joachim@teamwire.eu>
2016-08-24 12:37:57 +0200

    Adapt VMWare build script for offline installation images

--- packer/build-vmware.sh
+++ packer/build-vmware.sh
@@ -1,10 +1,72 @@
 #!/bin/bash -e
 
 WORKDIR=${0%/*}
-PASSWORD="$1"
+
+PASSWORD=""
+BACKEND_RELEASE=""
+DOCKERHUB_USERNAME=""
+DOCKERHUB_PASSWORD=""
+OFFLINE_INSTALLATION=""
+
+help() {
+	echo "Build Teamwire Server VMWare VM images"
+	echo
+	echo "Command line options:"
+	echo "  --password <password>            Define the password of the 'teamwire' user (mandantory)"
+	echo "  --offline-installation           Prepare an image for offline installation"
+	echo "                                   Requires the following three parameters"
+	echo "  --backend-release <TAG>          Backend release to integrate into the offline installation VM"
+	echo "  --dockerhub-username <username>  Docker Hub user name"
+	echo "  --dockerhub-password <password>  Docker Hub password"
+}
+
+while [[ $# -gt 0 ]] ; do
+	case "$1" in
+		--help|-h)
+			help
+			exit
+			;;
+		--password)
+			PASSWORD="$2"
+			shift
+			;;
+		--backend-release)
+			BACKEND_RELEASE="$2"
+			shift
+			;;
+		--dockerhub-username)
+			DOCKERHUB_USERNAME="$2"
+			shift
+			;;
+		--dockerhub-password)
+			DOCKERHUB_PASSWORD="$2"
+			shift
+			;;
+		--offline-installation)
+			OFFLINE_INSTALLATION=true
+			;;
+	esac
+	shift
+done
 
 if [ -z "$PASSWORD" ] ; then
 	echo "Please supply the password for the teamwire user on the command line."
+	echo
+	help
+	exit 1
+fi
+
+if [ -n "$OFFLINE_INSTALLATION" ] && [ -z "$DOCKERHUB_USERNAME" ] && [ -z "$DOCKERHUB_PASSWORD" ] ; then
+	echo "Please specify Docker Hub credentials for offline installations."
+	echo
+	help
+	exit 1
+fi
+
+if [ -n "$OFFLINE_INSTALLATION" ] && [ -z "$BACKEND_RELEASE" ] ; then
+	echo "Please specify a backend release when packaging for offline installation."
+	echo
+	help
 	exit 1
 fi
 
@@ -19,8 +81,12 @@ echo directory part: "$WORKDIR"
 pushd "$WORKDIR" > /dev/null
 packer build \
 	-var "http_directory=." \
-	-var "ssh_password=$PASSWORD" \
-    -only teamwire-server-vmware \
+	-var "ssh_password=${PASSWORD}" \
+	-var "offline_installation=${OFFLINE_INSTALLATION}" \
+	-var "backend_release=${BACKEND_RELEASE}" \
+	-var "dockerhub_password=${DOCKERHUB_PASSWORD}" \
+	-var "dockerhub_username=${DOCKERHUB_USERNAME}" \
+	-only teamwire-server-vmware \
 	"teamwire-server-debian.json"
 popd > /dev/null
 
