a41af338d839acf3cdc81e173830a0e20c32c219
Joachim de Groot <joachim@teamwire.eu>
2017-03-21 13:32:22 +0100

    Fix filter failure for addressless interfaces

--- ansible/playbooks/filter_plugins/addr_to_if.py
+++ ansible/playbooks/filter_plugins/addr_to_if.py
@@ -1,5 +1,8 @@
 def addr_to_if(addr, facts):
     for interface in facts['ansible_interfaces']:
+        if not 'ipv4' in interface:
+            continue
+
         if facts['ansible_{}'.format(interface)]['ipv4']['address'] == addr:
             return interface
 
