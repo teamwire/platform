397e9757672576ab84df848e7f5f7adf0b790fc3
Rebecca Dieter <rebecca@teamwire.eu>
2024-01-12 10:28:58 +0100

    Replace tabs and fix shellcheck errors in packer/scripts/vm-guest-tools.sh

--- packer/scripts/vm-guest-tools.sh
+++ packer/scripts/vm-guest-tools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
-	echo "Installing VMware Tools"
-	sudo DEBIAN_FRONTEND=noninteractive apt-get install -qqy --no-install-recommends open-vm-tools
-elif [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
-	echo "Installing VirtualBox Guest Additions"
-	sudo DEBIAN_FRONTEND=noninteractive apt-get install -qqy --no-install-recommends virtualbox-guest-dkms virtualbox-guest-utils
+if [[ ${PACKER_BUILDER_TYPE} =~ vmware ]]; then
+  echo "Installing VMware Tools"
+  sudo DEBIAN_FRONTEND=noninteractive apt-get install -qqy --no-install-recommends open-vm-tools
+elif [[ ${PACKER_BUILDER_TYPE} =~ virtualbox ]]; then
+  echo "Installing VirtualBox Guest Additions"
+  sudo DEBIAN_FRONTEND=noninteractive apt-get install -qqy --no-install-recommends virtualbox-guest-dkms virtualbox-guest-utils
 fi
