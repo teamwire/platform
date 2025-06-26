bf16d420452b05fab0ea98f6c50ea09aece1745b
Rebecca Dieter <rebecca@teamwire.eu>
2025-06-26 09:09:16 +0200

    Ensure to define nomad version variables via general-facts file

--- packer/scripts/prepare-offline-installation.sh
+++ packer/scripts/prepare-offline-installation.sh
@@ -187,7 +187,6 @@ REMOTE_URL=$(jq -r '.general.url' /etc/ansible/facts.d/general_facts.fact)
 
 CONSUL_VERSION=$(awk '/^consul_version:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/consul/vars/main.yml)
 CONSUL_TEMPLATE_VERSION=$(awk '/^consul_template_version:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/frontend/vars/main.yml)
-NOMAD_VERSION=$(awk '/^nomad_version:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/nomad/vars/main.yml)
 VAULT_VERSION=$(awk '/^vault_version:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/vault/vars/main.yml)
 CHECKMK_VERSION=$(jq -r '.monitoring.version' /etc/ansible/facts.d/general_facts.fact)
 CHECKMK_REMOTE_PATH=$(jq -r '.monitoring.path' /etc/ansible/facts.d/general_facts.fact)
@@ -217,7 +216,7 @@ harbor.teamwire.eu/teamwire/webclient:${WEBCLIENT_RELEASE}
 DOWNLOADS="
 https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip;$(awk '/^consul_checksum:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/consul/vars/main.yml)
 https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip;$(awk '/^consul_template_checksum:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/frontend/vars/main.yml)
-https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip;$(awk '/^nomad_checksum:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/nomad/vars/main.yml)
+$(jq -r '.nomad.url' /etc/ansible/facts.d/general_facts.fact);$(jq -r '.nomad.sha256' /etc/ansible/facts.d/general_facts.fact)
 https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip;$(awk '/^vault_checksum:/ { gsub("\"",""); print $2 }' ~teamwire/platform/ansible/roles/vault/vars/main.yml)
 ${REMOTE_URL}/external${CHECKMK_REMOTE_PATH}/check-mk-raw-${CHECKMK_VERSION}.${VERSION_CODENAME}_amd64.deb;$(curl -Ls "${REMOTE_URL}/service/rest/v1/search?repository=external&name=${CHECKMK_REMOTE_PATH}/check-mk-raw-${CHECKMK_VERSION}.${VERSION_CODENAME}_amd64.deb" | jq -r '.items[].assets[].checksum.sha256')
 ${REMOTE_URL}/repository/external${CHECKMK_SSLCERTIFICATE_REMOTE_PATH}/sslcertificates-${CHECKMK_SSLCERTIFICATE_VERSION}.mkp;$(curl -Ls "${REMOTE_URL}/service/rest/v1/search?repository=external&name=${CHECKMK_SSLCERTIFICATE_REMOTE_PATH}/sslcertificates-${CHECKMK_SSLCERTIFICATE_VERSION}.mkp" | jq -r '.items[].assets[].checksum.sha256')
